572e708604b8a0339a0ce8578a8cdce51e5920af
eneotujoe <eneotujoe@gmail.com>
2023-09-02 10:10:35 +0100

    rebuilt

--- consts.ts
+++ consts.ts
@@ -1,5 +1,5 @@
 export const SITE_TITLE = 'ABARASI HART';
-export const SITE_DESCRIPTION = '';
+export const SITE_DESCRIPTION = 'Dr Abarasi Abbey-Hart is a multidisciplinary research scientist. He holds a BEng (Chemical and Petrochemical Engineering), MSc (Advanced Chemical Engineering) and PhD in Chemical Engineering, with over 6 years of integrated professional experience spanning industry, teaching, consultancy, research and development, and professional training in the oil and gas industry and academia. As a research scientist he has worked on a broad range of Heavy oil recovery and upgrading techniques, gas analysis, catalyst preparation and characterisation, hydrodesulphurisation and hydrocracking reactions, biomass conversion technologies and particle technology in the UK and Nigeria. He is currently a Senior Research Fellow at the Department of Chemical and Biological Engineering at the University of Sheffield, where he is working with the FutureCat team on Next Generation Cathode Materials for Lithium-ion Batteries.';
 export const NAV_ITEMS = [
   ['Home', '/'],
   ['Publication', '/publication'],
